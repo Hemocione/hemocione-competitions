94b73dbadb51298f1320ef56a457b374cd9c1567
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-30 23:45:25 -0300

    Add erro handling

--- server/api/v1/competitions/[slug]/promotions/index.post.ts
+++ server/api/v1/competitions/[slug]/promotions/index.post.ts
@@ -8,6 +8,12 @@ export default defineEventHandler(async (event) => {
     name
   } = await readBody(event);
 
+  if (!hemocioneId || !name) throw createError({
+    statusCode: 400,
+    statusMessage:
+      "Bad Request - name and hemocioneId are required",
+  });
+
   const competitionSlug = String(getRouterParam(event, 'slug'));
 
   return sendPromotion(hemocioneId, name, competitionSlug);
