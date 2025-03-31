5fe0a2c8c212e0415d8ece41e806454a25c1caf7
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-03-31 18:48:09 -0300

    create hemocioneWebHookService

--- server/services/hemocioneWebHookService.ts
+++ server/services/hemocioneWebHookService.ts
@@ -1,6 +1,6 @@
 const config = useRuntimeConfig();
 
-export async function sendPromotion(hemocioneId: string, name: string, competitionSlug: string) {
+export async function callWebhook(hemocioneId: string, name: string, competitionSlug: string) {
   return await $fetch(`${config.public.hemocionePromotionsApiUrl}/${competitionSlug}/trigger-async`, {
     method: "POST",
     body: {
