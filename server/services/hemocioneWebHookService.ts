b5a44304c859673386fd4aa6d6e2060f7961d9d7
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-01 08:57:58 -0300

    change secret to be queryparams

--- server/services/hemocioneWebHookService.ts
+++ server/services/hemocioneWebHookService.ts
@@ -1,7 +1,7 @@
 const config = useRuntimeConfig();
 
 export async function callWebhook(hemocioneId: string, name: string, competitionSlug: string) {
-  return await $fetch(`${config.public.hemocionePromotionsApiUrl}/${competitionSlug}/trigger-async`, {
+  return await $fetch(`${config.public.hemocionePromotionsApiUrl}/${competitionSlug}/trigger-async?x-secret${config.hemocionePromotionsApiSecret}`, {
     method: "POST",
     body: {
       hemocioneId,
@@ -9,7 +9,6 @@ export async function callWebhook(hemocioneId: string, name: string, competition
     },
     headers: {
       "Content-Type": "application/json",
-      "x-secret": config.hemocionePromotionsApiSecret,
     }
   })
 }
