9d454172aebdf5eae6c221314f5f501e80b2b3be
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2025-04-01 10:05:56 -0300

    remove secure treatment

--- server/api/v1/competitions/[slug]/index.put.ts
+++ server/api/v1/competitions/[slug]/index.put.ts
@@ -46,11 +46,5 @@ export default defineEventHandler(async (event) => {
     autoApprove
   });
 
-  const {
-    autoApprove: autoApproveRemoved,
-    webhook_configs: webhookConfigsRemoved,
-    ...secureCompetition
-  } = editedCompetition;
-
-  return secureCompetition;
+  return editedCompetition
 });
