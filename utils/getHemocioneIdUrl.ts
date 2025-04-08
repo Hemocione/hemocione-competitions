197c109b5e666f3c6f4c74e39b082893dfeb7dba
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2025-04-08 16:25:55 -0300

    refactor: improve URL construction in getHemocioneIdUrl function

--- utils/getHemocioneIdUrl.ts
+++ utils/getHemocioneIdUrl.ts
@@ -2,7 +2,10 @@ export function getHemocioneIdUrl(redirectUrl: string): string {
   const config = useRuntimeConfig();
 
   const encodedRedirectUrl = encodeURIComponent(redirectUrl);
-  const finalUrl = `${config.public.hemocioneIdUrl}?redirect=${encodedRedirectUrl}`;
+  const hemocioneIdUrl = config.public.hemocioneIdUrl.endsWith("/")
+    ? config.public.hemocioneIdUrl
+    : `${config.public.hemocioneIdUrl}/`;
+  const finalUrl = `${hemocioneIdUrl}?redirect=${encodedRedirectUrl}`;
   console.log("Hemocione ID URL:", finalUrl);
   return finalUrl;
 }
