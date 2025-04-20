a483bc60ada65a0dbebf8d18dbd7a06cbbe345ea
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2025-04-19 22:18:47 -0300

    Changed route structure + Add call do webhook when status is approved

--- server/api/v1/competitions/[slug]/donations/[donationId]/status/index.put.ts
+++ server/api/v1/competitions/[slug]/donations/[donationId]/status/index.put.ts
@@ -1,5 +1,8 @@
 import { assertSecretAuth } from "~/server/services/auth";
 import { updateDonationStatus } from "~/server/services/donationService";
+import { getCompetitionBySlug } from "~/server/services/competitionService";
+import { waitUntil } from '@vercel/functions';
+import { callWebhook } from "~/server/services/webhookService";
 
 export default defineEventHandler(async (event) => {
   assertSecretAuth(event);
@@ -26,6 +29,19 @@ export default defineEventHandler(async (event) => {
     donationId,
     status,
   });
+
+  const competitionSlug = String(getRouterParam(event, 'slug'));
+  const competition = await getCompetitionBySlug(competitionSlug);
+  if (!competition) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Competition not found"
+    });
+  }
+
+  if (updatedDonation.status === "approved" && competition.webhook_configs?.donation_approved) {
+    waitUntil(callWebhook(competition.webhook_configs?.donation_approved, { hemocioneId: updatedDonation.hemocioneID }));
+  }
 
   return updatedDonation
 
